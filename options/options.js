ae74657365ea39369cd50bbf5a894c302e695ee4
Crowdbotics <team@crowdbotics.com>
2022-04-06 10:46:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "tcl_pay_34624",
+  url: "https://tcl_pay_34624.botics.co",
+  api: "https://tcl_pay_34624.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
